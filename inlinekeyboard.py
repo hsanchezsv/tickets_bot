46599210e9cec807a9e04a36ef3b0876bbe22f60
Heinrich Sanchez <hsanchez.sv@gmail.com>
2018-06-18 15:10:18 -0600

    "Message Handler, comandos no reconocidos"

--- inlinekeyboard.py
+++ inlinekeyboard.py
@@ -6,6 +6,7 @@
 import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram.ext import MessageHandler, Filters
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -13,23 +14,24 @@ logger = logging.getLogger(__name__)
 
 
 def start(bot, update):
-    keyboard = [[InlineKeyboardButton("Ver Tickets", callback_data='1'),
-                 InlineKeyboardButton("Abrir", callback_data='2')],
+    #keyboard = [[InlineKeyboardButton("Ver Tickets", callback_data='1'),
+    #             InlineKeyboardButton("Abrir", callback_data='2')],
 
-                [InlineKeyboardButton("Option 3", callback_data='3')]]
+    #            [InlineKeyboardButton("Option 3", callback_data='3')]]
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    #reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+    update.message.reply_text('Escriba estas opciones: \n/open Escriba Texto Describiendo el Ticket \n /tickets (Para ver la lista de tickets abiertos)')
 
 
 def button(bot, update):
     query = update.callback_query
 
-    bot.edit_message_text(text="Selected option: {}".format(query.data),
-                          chat_id=query.message.chat_id,
-                          message_id=query.message.message_id)
-
+    #bot.edit_message_text(text="Selected option: {}".format(query.data),
+    #                      chat_id=query.message.chat_id,
+    #                      message_id=query.message.message_id)
+    if query.data == '1':
+        update.message.reply_text("Los tickes abiertos actualmente son: ")
 
 def help(bot, update):
     update.message.reply_text("Use /start to test this bot.")
@@ -39,21 +41,27 @@ def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
-def open(bot, update):
-    update.message.reply_text("Use /open y una descripcion dele ticket.")
+def unknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Sorry, No se reconoce este comando")
+
+def open(bot, update, args):
+    query = update.callback_query
+    if query.data == '1':
+        update.message.reply_text("Use /open y una descripcion del ticket.")
+    update.message.reply_text("")
 
 def main():
     # Create the Updater and pass it your bot's token.
     updater = Updater("612717632:AAHKiLydjhDd1DF7y4DJIyn1gwJbhkhUnmY")
 
     updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    #updater.dispatcher.add_handler(CallbackQueryHandler(button))
     updater.dispatcher.add_handler(CommandHandler('help', help))
     updater.dispatcher.add_error_handler(error)
 
     #comandos para Tickets MB
     updater.dispatcher.add_handler(CommandHandler('open', open))
-
+    updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
     # Start the Bot
     updater.start_polling()
 
